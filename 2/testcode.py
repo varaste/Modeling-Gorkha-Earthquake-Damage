ec54290949380aa17001c4d53c0f07d1259fbde5
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-08-05 10:38:42 +0430

    Adding test for function ISOLAation
    Signed-off-by: Arya Varaste <30382009+varaste@users.noreply.github.com>

---
+++ 2/testcode.py
@@ -0,0 +1,8 @@
+# Feel free to modify this code for testing, however the "submit" button
+# will NOT recognize any of these changes
+
+from gamestate import *
+
+print("Creating empty game board...")
+emptyState = GameState()
+print("Everything looks good!")
