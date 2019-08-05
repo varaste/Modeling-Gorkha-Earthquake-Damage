459ec96c53a5d0f6d44999c445960f3de7134ee1
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-08-05 16:32:55 +0430

    Add testcode2.py
    Signed-off-by: Arya Varaste <30382009+varaste@users.noreply.github.com>

---
+++ 2/testcode2.py
@@ -0,0 +1,13 @@
+import minimax_helpers
+
+from gamestate import *
+
+g = GameState()
+
+print("Calling min_value on an empty board...")
+v = minimax_helpers.min_value(g)
+
+if v == -1:
+    print("min_value() returned the expected score!")
+else:
+    print("Uh oh! min_value() did not return the expected score.")
