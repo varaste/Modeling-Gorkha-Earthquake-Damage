01668376046243f44a8d6774545bf14cc73f5e77
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-08-05 16:33:55 +0430

    add minimaz_helpers.py
    Signed-off-by: Arya Varaste <30382009+varaste@users.noreply.github.com>

---
+++ 2/minimax_helpers.py
@@ -0,0 +1,24 @@
+def terminal_test(gameState):
+    """ Return True if the game is over for the active player
+    and False otherwise.
+    """
+    # TODO: finish this function!
+    pass
+
+
+def min_value(gameState):
+    """ Return the value for a win (+1) if the game is over,
+    otherwise return the minimum value over all legal child
+    nodes.
+    """
+    # TODO: finish this function!
+    pass
+
+
+def max_value(gameState):
+    """ Return the value for a loss (-1) if the game is over,
+    otherwise return the maximum value over all legal child
+    nodes.
+    """
+    # TODO: finish this function!
+    pass
