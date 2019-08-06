e91711e24c6c23dfc0eaefc08d44086c37e4f138
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-08-06 18:53:01 +0430

    update solution.py
    Signed-off-by: Arya Varaste <30382009+varaste@users.noreply.github.com>

--- 2/solution.py
+++ 2/solution.py
@@ -1,32 +1,37 @@
+from minimax_helpers import *
 
-def terminal_test(gameState):
-    """ Return True if the game is over for the active player
-    and False otherwise.
+
+# Solution using an explicit loop based on max_value()
+def _minimax_decision(gameState):
+	""" Return the move along a branch of the game tree that
+    has the best possible value.  A move is a pair of coordinates
+    in (column, row) order corresponding to a legal move for
+    the searching player.
+
+    You can ignore the special case of calling this function
+    from a terminal state.
     """
-    return not bool(gameState.get_legal_moves())  # by Assumption 1
+	best_score = float("-inf")
+	best_move = None
+	for m in gameState.get_legal_moves():
+		v = min_value(gameState.forecast_move(m))
+		if v > best_score:
+			best_score = v
+			best_move = m
+	return best_move
 
 
-def min_value(gameState):
-    """ Return the value for a win (+1) if the game is over,
-    otherwise return the minimum value over all legal child
-    nodes.
+# This solution does the same thing using the built-in `max` function
+# Note that "lambda" expressions are Python's version of anonymous functions
+def minimax_decision(gameState):
+	""" Return the move along a branch of the game tree that
+    has the best possible value.  A move is a pair of coordinates
+    in (column, row) order corresponding to a legal move for
+    the searching player.
+
+    You can ignore the special case of calling this function
+    from a terminal state.
     """
-    if terminal_test(gameState):
-        return 1  # by Assumption 2
-    v = float("inf")
-    for m in gameState.get_legal_moves():
-        v = min(v, max_value(gameState.forecast_move(m)))
-    return v
-
-
-def max_value(gameState):
-    """ Return the value for a loss (-1) if the game is over,
-    otherwise return the maximum value over all legal child
-    nodes.
-    """
-    if terminal_test(gameState):
-        return -1  # by assumption 2
-    v = float("-inf")
-    for m in gameState.get_legal_moves():
-        v = max(v, min_value(gameState.forecast_move(m)))
-    return v
+	# The built in `max()` function can be used as argmax!
+	return max(gameState.get_legal_moves(),
+			   key=lambda m: min_value(gameState.forecast_move(m)))
