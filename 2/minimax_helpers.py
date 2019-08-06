b98602bf9ade290226d5e4be43b8c5aba6ae18a2
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-08-06 18:49:47 +0430

    update min_valie in minimaz_helpers.py
    Signed-off-by: Arya Varaste <30382009+varaste@users.noreply.github.com>

--- 2/minimax_helpers.py
+++ 2/minimax_helpers.py
@@ -11,8 +11,12 @@ def min_value(gameState):
     otherwise return the minimum value over all legal child
     nodes.
     """
-    # TODO: finish this function!
-    pass
+    if terminal_test(gameState):
+        return gameState.utility(0)
+    v = float("inf")
+    for m in gameState.get_legal_moves():
+        v = min(v, max_value(gameState.forecast_move(m)))
+    return v
 
 
 def max_value(gameState):
@@ -20,5 +24,9 @@ def max_value(gameState):
     otherwise return the maximum value over all legal child
     nodes.
     """
-    # TODO: finish this function!
-    pass
+    if terminal_test(gameState):
+        return gameState.utility(0)
+    v = float("-inf")
+    for m in gameState.get_legal_moves():
+        v = max(v, min_value(gameState.forecast_move(m)))
+    return v
